adf3070ff21e6594ddf37df41417fa8e11dfcaf3
felip <felip@DESKTOP-0VT61A2>
2023-03-26 14:05:06 -0300

    Adicionar arquivos de projeto.

---
+++ atividade 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome da pessoa:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o sexo da pessoa (M/F):");
+        char sexo = char.Parse(Console.ReadLine());
+
+        if (sexo == 'M' || sexo == 'm')
+        {
+            Console.WriteLine("Ilmo Sr. " + nome);
+        }
+        else if (sexo == 'F' || sexo == 'f')
+        {
+            Console.WriteLine("Ilma Sra. " + nome);
+        }
+        else
+        {
+            Console.WriteLine("Sexo inválido");
+        }
+    }
+}
+//atividade 3//
